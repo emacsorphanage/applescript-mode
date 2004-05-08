ecc478acfca722d324726bc611518d7e9714b409
sakito <sakito@e12f0c66-1e22-0410-9236-8a0cd71f8e0c>
2004-05-08 10:54:33 +0000

    more color

--- applescript-mode.el
+++ applescript-mode.el
@@ -1,7 +1,8 @@
-;;; applescript-mode.el --- AppleScript Mode
+;;; applescript-mode.el --- major mode for editing AppleScript source
 
 ;; Copyright (C) 2004  MacEmacs JP Project
 
+;;; Credits:
 ;; Copyright (C) 2003,2004 FUJIMOTO Hisakuni
 ;;   http://www.fobj.com/~hisa/w/applescript.el.html
 ;; Copyright (C) 2003 443,435 (Public Domain?)
@@ -9,11 +10,14 @@
 ;; Copyright (C) 2004 Harley Gorrell <harley@mahalito.net>
 ;;   http://www.mahalito.net/~harley/elisp/osx-osascript.el
 
-;; Author: sakito <sakito@s2.xrea.com>
+;; Author: sakito <sakito@users.sourceforge.jp>
 ;; Keywords: languages, tools
 
-(defvar applescript-mode-version "$Revision$"
+(defconst applescript-mode-version "$Revision$"
   "The current version of the AppleScript mode.")
+
+(defconst applescript-mode-help-address "sakito@users.sourceforge.jp"
+  "Address accepting submission of bug reports.")
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -34,6 +38,13 @@
 
 ;; AppleScript Mode
 
+;;; Usage:
+;; To use applescript-mode.el put the following line in your .emacs:
+;; (autoload 'applescript-mode "applescript-mode" "major mode for editing AppleScript source." t)
+;; (setq auto-mode-alist
+;;      (cons '("\\.applescript$" . applescript-mode) auto-mode-alist)
+;;      )
+
 ;; Please use the SourceForge MacEmacs JP Project to submit bugs or
 ;; patches:
 ;;
@@ -47,6 +58,8 @@
 
 
 ;;; Code:
+
+
 
 ;; user customize variables
 (defgroup applescript nil
@@ -89,24 +102,63 @@ for a block opening statement are given this extra offset."
 ;; Face for true, false, me, it
 (defvar as-pseudo-keyword-face 'as-pseudo-keyword-face
   "Face for pseudo keywords in AppleScript mode, like me, true, false.")
-(make-face 'applescript-pseudo-keyword-face)
+(make-face 'as-pseudo-keyword-face)
+
+;; Face for command
+(defvar as-command-face 'as-command-face
+  "Face for command like copy, get, set, and error.")
+(make-face 'as-command-face)
+
+(defun as-font-lock-mode-hook ()
+  (or (face-differs-from-default-p 'as-pseudo-keyword-face)
+      (copy-face 'font-lock-keyword-face 'as-pseudo-keyword-face))
+  (or (face-differs-from-default-p 'as-command-face)
+      (copy-face 'font-lock-keyword-face 'as-command-face))
+  )
+(add-hook 'font-lock-mode-hook 'as-font-lock-mode-hook)
 
 (defvar applescript-font-lock-keywords
-  (let ((kw1 (mapconcat 'identity
-                        '("activate" "and" "app" "application" "considering"
-                          "else" "end" "exit" "launch" "on"
-                          "if" "ignoring" "reopen" "repeat" "run"
+  (let (
+        ;; expressions and control Statements
+        (kw1 (mapconcat 'identity
+                        '("and" "app" "application" "considering" "div"
+                          "else" "end" "exit" "is" "mod" "not" "on" "or"
+                          "if" "ignoring" "reopen" "repeat"
                           "tell" "then" "to"
+                          "using[ \t]terms[ \t]from"
+                          "with[ \t]timeout" "with[ \t]transaction"
                           )
                         "\\|"))
+        ;; commands
         (kw2 (mapconcat 'identity
-                        '("error" "try")
+                        '("ASCII[ \t]character" "ASCII[ \t]number" "activate" "AGStart"
+                          "beep"  "copy" "count" "choose[ \t]application"
+                          "choose[ \t]file" "choose[ \t]folder" "close[ \t]access"
+                          "current[ \t]date" "display[ \t]dialog" "get" "get[ \t]EOF"
+                          "info[ \t]for" "launch" "list[ \t]disks" "list[ \t]folder"
+                          "load[ \t]script" "log" "monitor[ \t]depth" "max[ \t]monitor[ \t]depth"
+                          "min[ \t]monitor[ \t]depth" "new[ \t]file" "offset"
+                          "open[ \t]for[ \t]access" "path[ \t]to" "random[ \t]number"
+                          "read" "round" "run" "run[ \t]script" "scripting[ \t]component"
+                          "set" "set[ \t]EOF" "set[ \t]monitor[ \t]depth" "set[ \t]volume"
+                          "start[ \t]log" "stop[ \t]log" "store[ \t]script"
+                          "time[ \t]to[ \t]GMT" "write"
+                          )
+                        "\\|"))
+        ;; misc
+        (kw3 (mapconcat 'identity
+                        '("buttons" "default[ \t]answer" "default[ \t]button"
+                          "to[ \t]begining[ \t]of" "to[ \t]word" "starting[ \t]at"
+                          "with[ \t]icon" "write[ \t]permission"
+                          )
                         "\\|"))
         )
     (list
      ;; keywords
      (cons (concat "\\b\\(" kw1 "\\)\\b[ \n\t(]") 1)
-     (cons (concat "\\b\\(" kw2 "\\)[ \n\t(]") 1)
+     (cons (concat "\\b\\(" kw2 "\\)\\b[ \n\t(]") 1)
+     ;; kw3 not yet..
+     (cons (concat "\\b\\(" kw3 "\\)\\b[ \n\t(]") 1)
      ;; functions
      '("\\bon[ \t]+\\([a-zA-Z_]+[a-zA-Z0-9_]*\\)"
        1 font-lock-function-name-face)
@@ -138,6 +190,7 @@ for a block opening statement are given this extra offset."
 
   ;; subprocess commands
   (define-key as-mode-map "\C-c\C-c" 'as-execute-buffer)
+  (define-key as-mode-map "\C-c\C-s" 'as-execute-string)
   (define-key as-mode-map "\C-c|" 'as-execute-region)
 
   ;; Miscellaneous
@@ -148,13 +201,43 @@ for a block opening statement are given this extra offset."
   ;(define-key as-mode-map "\C-c\C-v" 'as-mode-version)
   )
 
-;; not yet
-;(defvar as-mode-syntax-table nil
-;  "Syntax table used in `applescript-mode' buffers.")
-;(when (not as-mode-syntax-table)
-;  (setq as-mode-syntax-table (make-syntax-table))
-;  (modify-syntax-entry ?\( "()" as-mode-syntax-table)
-;  )
+(defvar as-mode-syntax-table nil
+  "Syntax table used in `applescript-mode' buffers.")
+(when (not as-mode-syntax-table)
+  (setq as-mode-syntax-table (make-syntax-table))
+
+  (modify-syntax-entry ?\" "\"" as-mode-syntax-table)
+
+  (modify-syntax-entry ?:  "." as-mode-syntax-table)
+  (modify-syntax-entry ?\; "." as-mode-syntax-table)
+  (modify-syntax-entry ?&  "." as-mode-syntax-table)
+  (modify-syntax-entry ?\|  "." as-mode-syntax-table)
+  (modify-syntax-entry ?+  "." as-mode-syntax-table)
+  (modify-syntax-entry ?*  "." as-mode-syntax-table)
+  (modify-syntax-entry ?/  "." as-mode-syntax-table)
+  (modify-syntax-entry ?=  "." as-mode-syntax-table)
+  (modify-syntax-entry ?<  "." as-mode-syntax-table)
+  (modify-syntax-entry ?>  "." as-mode-syntax-table)
+  (modify-syntax-entry ?$ "." as-mode-syntax-table)
+  (modify-syntax-entry ?\[ "." as-mode-syntax-table)
+  (modify-syntax-entry ?\] "." as-mode-syntax-table)
+  (modify-syntax-entry ?\{ "." as-mode-syntax-table)
+  (modify-syntax-entry ?\} "." as-mode-syntax-table)
+  (modify-syntax-entry ?. "." as-mode-syntax-table)
+  (modify-syntax-entry ?\\ "." as-mode-syntax-table)
+  (modify-syntax-entry ?\' "." as-mode-syntax-table)
+
+  ;; a double hyphen starts a comment
+  (modify-syntax-entry ?-  ". 12" as-mode-syntax-table)
+
+  ;; comment delimiters
+  (modify-syntax-entry ?\f  ">   " as-mode-syntax-table)
+  (modify-syntax-entry ?\n  ">   " as-mode-syntax-table)
+
+  ;; define parentheses to match
+  (modify-syntax-entry ?\( "()" as-mode-syntax-table)
+  (modify-syntax-entry ?\) ")(" as-mode-syntax-table)
+  )
 
 ;; Utilities
 (defmacro as-safe (&rest body)
@@ -190,8 +273,8 @@ This function does not modify point or mark."
     (cond
      ((eq position 'bol) (beginning-of-line))
      ((eq position 'eol) (end-of-line))
-     ((eq position 'bod) (as-beginning-of-on-or-to 'either))
-     ((eq position 'eod) (as-end-of-on-or-to 'either))
+     ((eq position 'bod) (as-beginning-of-handler 'either))
+     ((eq position 'eod) (as-end-of-handler 'either))
      ;; Kind of funny, I know, but useful for as-up-exception.
      ((eq position 'bob) (beginning-of-buffer))
      ((eq position 'eob) (end-of-buffer))
@@ -215,13 +298,15 @@ contain this package.")
      (easy-menu-define
       as-menu as-mode-map "AppleScript Mode menu"
       '("AppleScript"
-        ["Comment Out Region"   comment-region t]
-        ["Uncomment Region"     uncomment-region t]
+        ["Comment Out Region"   comment-region (mark)]
+        ["Uncomment Region"     uncomment-region (mark)]
         "-"
         ["Execute buffer"       as-execute-buffer t]
-        ["Execute region"       as-execute-region t]
+        ["Execute region"       as-execute-region (mark)]
+        ["Execute string"       as-execute-string t]
         "-"
-        
+        ["Mode Version"         as-mode-version t]
+        ["AppleScript Version"   as-language-version t]
         )))
 
 ;;;###autoload
@@ -248,19 +333,26 @@ contain this package.")
   ;; Maps and tables
   (use-local-map as-mode-map)
 
+  (set-syntax-table as-mode-syntax-table)
+
   ;; Names
   (setq major-mode 'applescript-mode
         mode-name "AppleScript"
         local-abbrev-table applescript-mode-abbrev-table
         font-lock-defaults '(applescript-font-lock-keywords)
-        paragraph-separate "^[ \t]*$"
-        paragraph-start    "^[ \t]*$"
+        paragraph-separate "[ \t\n\f]*$"
+        paragraph-start    "[ \t\n\f]*$"
         require-final-newline t
-        comment-start "(* "
-        comment-end   " *)"
-        comment-start-skip "(\\*+ *"
+        comment-start "-- "
+        comment-end   ""
+        comment-start-skip "---*[ \t]*"
         comment-column 40
         )
+
+  ;;  Support for outline-minor-mode
+  (set (make-local-variable 'outline-regexp)
+       "\\([ \t]*\\(on\\|to\\|if\\|repeat\\|tell\\|end\\)\\|--\\)")
+  (set (make-local-variable 'outline-level) 'as-outline-level)
 
   ;; add the menu
   (if as-menu
@@ -279,34 +371,61 @@ contain this package.")
 ;;; Subprocess commands
 
 ;; function use variables
-(defvar as-last-execute nil
-  "Stores the last Applescript command executed from Emacs.")
-
 (defconst as-output-buffer "*AppleScript Output*"
   "Name for the buffer to display AppleScript output.")
 (make-variable-buffer-local 'as-output-buffer)
 
-(defun as-execute-buffer ()
+;; Code execution commands
+(defun as-execute-buffer (&optional async)
   "Execute the whole buffer as an Applescript"
-  (interactive)
-  (setq as-last-execute (buffer-string))
-  (as-execute-code (buffer-string)))
+  (interactive "P")
+  (as-execute-region (point-min) (point-max) async))
 
-(defun as-execute-region ()
+(defun as-execute-string (string &optional async)
+  "Send the argument STRING to an AppleScript."
+  (interactive "sExecute AppleScript: ")
+  (save-excursion
+    (set-buffer (get-buffer-create
+                 (generate-new-buffer-name as-output-buffer)))
+    (insert string)
+    (as-execute-region (point-min) (point-max) async)))
+
+(defun as-execute-region (start end &optional async)
   "Execute the region as an Applescript"
-  (interactive)
-  (let ((region (buffer-substring (region-beginning) (region-end))))
-    (setq as-last-execute region)
-    (as-execute-code region)))
+  (interactive "r\nP")
+  (let ((region (buffer-substring start end))
+        (as-current-win (selected-window)))
+    (pop-to-buffer as-output-buffer)
+    (insert (as-execute-code region))
+    (select-window as-current-win)
+    ))
 
 (defun as-execute-code (code)
   "pop to the AppleScript buffer, run the code and display the results."
-  (let ((as-current-win (selected-window)))
-    (pop-to-buffer as-output-buffer)
-    (insert (osx-string-to-sjis-string-with-escape code))
-    (insert (as-decode-string (do-applescript (osx-string-to-sjis-string-with-escape code))))
-    (select-window as-current-win)
-    ))
+  (as-decode-string (do-applescript (as-string-to-sjis-string-with-escape code))))
+
+(defun as-mode-version ()
+  "Echo the current version of `applescript-mode' in the minibuffer."
+  (interactive)
+  (message "Using `applescript-mode' version %s" applescript-mode-version)
+  (as-keep-region-active))
+
+(defun as-language-version()
+  "Echo the current version of AppleScript Version in the minibuffer."
+  (interactive)
+  (message "Using AppleScript version %s" (as-execute-code "AppleScript's version"))
+  (as-keep-region-active))
+
+;; as-beginning-of-handler, as-end-of-handler,as-goto-initial-line not yet
+
+;;  Support for outline.el
+(defun as-outline-level ()
+  "This is so that `current-column` DTRT in otherwise-hidden text"
+  ;; patch from Dave Love <fx@gnu.org>
+  (let (buffer-invisibility-spec)
+    (save-excursion
+      (back-to-indentation)
+      (current-column))))
 
 ;; for Japanese
 (defun as-unescape-string (str)
@@ -317,19 +436,19 @@ contain this package.")
   "add escape char '\'"
   (replace-regexp-in-string "\\\\" "\\\\\\\\" str))
 
-(defun osx-sjis-byte-list-escape (lst)
+(defun as-sjis-byte-list-escape (lst)
   (cond
    ((null lst) nil)
    ((= (car lst) 92)
-    (append (list 92 (car lst)) (osx-sjis-byte-list-escape (cdr lst))))
-   (t (cons (car lst) (osx-sjis-byte-list-escape (cdr lst))))))
+    (append (list 92 (car lst)) (as-sjis-byte-list-escape (cdr lst))))
+   (t (cons (car lst) (as-sjis-byte-list-escape (cdr lst))))))
 
-(defun osx-string-to-sjis-string-with-escape (str)
+(defun as-string-to-sjis-string-with-escape (str)
   "String convert to SJIS, and escape \"\\\" "
   (apply 'string
-         (osx-sjis-byte-list-escape
+         (as-sjis-byte-list-escape
           (string-to-list
-           (encode-coding-string str 'sjis)))))
+           (as-encode-string str)))))
 
 (defun as-decode-string (str)
   "do-applescript return values decode sjis-mac"
@@ -365,35 +484,5 @@ contain this package.")
     ;; else
     (t (intern retstr))))
 
-
-(defconst as-help-address "sakito@users.sourceforge.jp"
-  "Address accepting submission of bug reports.")
-
-(defun as-mode-version ()
-  "Echo the current version of `applescript-mode' in the minibuffer."
-  (interactive)
-  (message "Using `applescript-mode' version %s" applescript-mode-version)
-  (as-keep-region-active))
-
-
-;; (require 'thingatpt)
-;; (defun jamming-lookup (word)
-;;   (do-applescript
-;;    (concat "tell application \"Jamming\" \r activate \r search \""
-;; 	   (string-to-sjis-string-with-escape (read-from-minibuffer "Jamming: " word))
-;; 	   "\" \r end tell")))
-
-;; (defun jamming-lookup-region (start end)
-;;   (interactive "r")
-;;   (jamming-lookup (buffer-substring-no-properties start end)))
-
-;; (defun jamming-lookup-word ()
-;;   (interactive)
-;;   (jamming-lookup (word-at-point)))
-
-;; (global-set-key "\M-l" 'jamming-lookup-word)
-
 (provide 'applescript-mode)
-;;; applescript-mode.el ends here
-ide 'applescript-mode)
 ;;; applescript-mode.el ends here
