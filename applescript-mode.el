42b3db3838821f240e05752de4337359d25d8c04
JenChieh <jcs090218@gmail.com>
2021-02-23 23:36:47 +0800

    Add version tag

--- applescript-mode.el
+++ applescript-mode.el
@@ -13,6 +13,7 @@
 
 ;; Author: sakito <sakito@users.sourceforge.jp>
 ;; Keywords: languages, tools
+;; Version: 0.1
 
 (defconst applescript-mode-version "$Revision$"
   "The current version of the AppleScript mode.")
